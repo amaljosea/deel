5ce3cee5aaaead3e0d32431413a8b6e20813054c
Amal Jose <amaljoseami@gmail.com>
2024-02-02 00:47:41 +0530

    Basic e2e

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,6 +1,11 @@
-describe('My First Test', () => {
+describe('Basic test', () => {
   it('Visits the app root url', () => {
     cy.visit('/');
     cy.contains('ion-header', 'Pay Slip');
+  });
+
+  it('Visits the payslip url', () => {
+    cy.visit('/payslip/1');
+    cy.contains('ion-button', 'Download Payslip');
   });
 });
