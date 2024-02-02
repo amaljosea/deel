4050c3fa0749ab6d47ff745d4cd27b29676bbf44
Amal Jose <amaljoseami@gmail.com>
2024-02-02 10:55:26 +0530

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -25,30 +25,12 @@ import './theme/variables.css';
 
 setupIonicReact();
 
-import { createAnimation } from '@ionic/react';
-
-export const RouterAnimation = (baseEl: HTMLElement, opts?: any) => {
-  const enteringAnimation = createAnimation()
-    .addElement(opts.enteringEl)
-    .fromTo('opacity', '0', '1')
-    .fromTo('transform', 'translateX(100%)', 'translateX(0%)')
-    .duration(600)
-    .easing('cubic-bezier(0.85, 0, 0.15, 1)');
-
-  const leavingAnimation = createAnimation()
-    .addElement(opts.leavingEl)
-    .fromTo('opacity', '1', '0')
-    .fromTo('transform', 'translateX(0%)', 'translateX(-100%)')
-    .duration(600)
-    .easing('cubic-bezier(0.85, 0, 0.15, 1)');
-
-  return createAnimation().addAnimation([enteringAnimation, leavingAnimation]);
-};
+import { routerAnimation } from './utils/RouterAnimation';
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonRouterOutlet animation={RouterAnimation}>
+      <IonRouterOutlet animation={routerAnimation}>
         <Route path='/' exact={true}>
           <Redirect to='/home' />
         </Route>
